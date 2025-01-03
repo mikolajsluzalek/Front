4d0300ed72cd41277e21699080a11fa97ad89961
mikolajsluzalek <mikolajsluzalek@gmail.com>
2025-01-03 13:16:56 +0100

    Dashboard complete

--- Front/Front.Client/Program.cs
+++ Front/Front.Client/Program.cs
@@ -1,14 +1,20 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-// Register HttpClient with the correct base address
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("http://localhost:8000/")
+});
 
-// Add MudBlazor services
+builder.Services.AddBlazoredLocalStorage();
+
 builder.Services.AddMudServices();
+
+
 
 await builder.Build().RunAsync();
 
